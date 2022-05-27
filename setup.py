353e2504af805977b1935b80a1ddf6c2e46ad767
John ZuHone <jzuhone@gmail.com>
2022-05-27 13:27:38 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='storms',
+      packages=['storms'],
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
+      description='Solar storm analysis for ACIS Ops',
+      author='John ZuHone',
+      author_email='john.zuhone@cfa.harvard.edu',
+      url='http://github.com/acisops/storms',
+      install_requires=["numpy>=1.12.1","requests","astropy"],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3'
+      ],
+      )
